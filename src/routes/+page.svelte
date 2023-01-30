f9d75c077712df5e7aad897be4f0ac3498f51831
Michael Wilson <michael_wilson@sil.org>
2023-01-30 16:36:41 +0930

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { Page, Button, setNotice, Snackbar } from '../../node_modules/@silintl/ui-components/index.mjs'
+  import '../lib/_index.scss'
+</script>
+
+<Page>
+  <h1>Sveltekit + UI Components Template</h1>
+
+  <Button raised on:click={() => setNotice('hi')}>Click me!</Button>
+
+  <Snackbar />
+</Page>
