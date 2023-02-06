09f72787afcafb5b3724ed5c240631b4e5e5aec3
Michael Wilson <michael_wilson@sil.org>
2023-02-07 08:22:15 +0930

    try something

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
   import { Page, Button, setNotice } from '@silintl/ui-components'
 
-  export let data: { detail: any }
+  export let data = {}
 </script>
 
 <Page>
   <h1>Sveltekit + UI Components Template</h1>
 
-  <Button raised on:click={() => setNotice(data.detail)}>Click me!</Button>
+  <Button raised on:click={() => setNotice(data?.detail)}>Click me!</Button>
 </Page>
